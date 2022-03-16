380d499b99d0ca41fcd5d3991694ac4596a9606c
Kirill Sabaev <privet.kakdela.1980@mail.ru>
2022-03-16 12:59:23 +0500

    Изменение названий миграций, с s в конце

--- database/migrations/2022_03_14_105308_create_friends_table.php
+++ database/migrations/2022_03_14_105308_create_friends_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFriendTable extends Migration
+class CreateFriendsTable extends Migration
 {
     /**
      * Run the migrations.
